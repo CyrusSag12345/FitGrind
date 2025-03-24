204c922e430f3cac800aa38a665f87f1b4c1cd82
Cyrus <cyruzjr1205@gmail.com>
2025-03-24 12:56:44 +0800

    Added on Muscle Gain

---
+++ app/src/main/java/com/example/fitgrind/Calesthenics.kt
@@ -0,0 +1,11 @@
+package com.example.fitgrind
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Calesthenics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calesthenics)
+    }
+}
--- app/src/main/java/com/example/fitgrind/DietManagementActivity.kt
+++ app/src/main/java/com/example/fitgrind/DietManagementActivity.kt
@@ -1,11 +1,21 @@
 package com.example.fitgrind
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class DietManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diet_management)
+
+        findViewById<Button>(R.id.btnCalesthenics).setOnClickListener {
+            startActivity(Intent(this, CalisthenicsActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnBodyBuilding).setOnClickListener {
+            startActivity(Intent(this, BodyBuildingActivity::class.java))
+        }
     }
 }
